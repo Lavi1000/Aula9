986573750384449cd56d3520fafa3a0dc6ec7ac1
Lavi1000 <145509294+Lavi1000@users.noreply.github.com>
2023-09-19 20:29:44 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Aula9
+# project_c9_pro_new
--- sketch.js
+++ sketch.js
@@ -1,30 +1,40 @@
-var box
-
 
 function setup() {
-  createCanvas(400, 400);
-  box = createSprite(150, 150, 50, 50);
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
-function draw() {
-  background(30);
+function draw() 
+{
 
-  if (keyIsDown(RIGHT_ARROW)) {
-    box.position.x = box.position.x + 5;
+  // escreva o código para alterar a cor de fundo 
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  if(keyIsDown(RIGHT_ARROW)){
+    background("purple");
   }
-  if(keyIsDown(LEFT_ARROW)){
-    box.position.x= box.position.x -5;
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("lightblue");
+    
   }
-  if(keyIsDown(UP_ARROW)){
-    box.position.y= box.position.y -5;
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("orange");
+   
   }
-  if(keyIsDown(DOWN_ARROW)){
-    box.position.y= box.position.y +5;
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("lightgreen");
   }
+
+
+  
   drawSprites();
-
 }
-
-
-
 
