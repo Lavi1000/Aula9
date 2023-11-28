b184dd719b71f6a45062e1fc919d327e5a1f50ce
Lavi1000 <145509294+Lavi1000@users.noreply.github.com>
2023-11-28 20:45:14 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(1000,1000);
+	createCanvas(1200,700);
 	engine = Engine.create();
 	world = engine.world;
 
@@ -25,13 +25,18 @@ function setup() {
 		density: 1.2
 	}
 
+	var options ={
+		isStatic: true
+	}
+
 	ball = Bodies.circle(200, 50, 20, ball_options);
 	World.add(world, ball);
 
-	groundObj = new Ground(width / 2, 670, width, 20);
-	leftSize = new Ground(1100, 600, 20, 120);
+	groundObj = new Ground(width / 2, 670, width, 20, options );
+	leftSize = new Ground(1100, 600, 20, 120 );
 	rightSize = new Ground(1200, 700, 20, 120);
-
+ 
+	
 
 	rectMode(CENTER);
 	ellipseMode(RADIUS);
@@ -41,16 +46,17 @@ function setup() {
 
 
 function draw() {
-	background(51);
 	drawSprites();
+	background(51);
+	Engine.update(engine);
 	ellipse(ball.position.x, ball.position.y, 20);
-
+	rect(leftSize.position.x, leftSize.position.y, 20, 120);
 	groundObj.display();
 	leftSize.display();
 	rightSize.display();
 
 
-	Engine.update(engine);
+	
 }
 
 function keyPressed() {
